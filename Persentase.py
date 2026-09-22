67f3bcbd3aabb964f399f4cb576319b0134c89ce
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-09-22 10:43:40 +0700

    Create Persentase student

--- Persentase.py
+++ Persentase.py
@@ -1,1 +1,10 @@
-persentase = float(input("Masukan Persentase Student performance"))
+persentase = float(input("Masukan Persentase Student performance = "))
+
+if persentase >= 90 :
+  print("Excellent performance")
+elif persentase >= 80 :
+  print("Very Good performance")
+elif persentase >=70 :
+  print("Good performance")
+else :
+  print("average performance")
